fbc4efcb7f37c33f2f360d9d24c56b0e8da27711
NishiOwO <nishi@nishi.boats>
2026-03-30 23:58:11 +0900

    fix thing

--- mk/module.mk
+++ mk/module.mk
@@ -12,7 +12,7 @@ mod_$(MODULE)$(A): $(OBJS)
 	touch ../../server/.relink
 
 mod_$(MODULE)$(SO): $(OBJS) ../../fpr/$(LIB)fpr$(A)
-	$(CC) $(LDFLAGS) $(RPATH) $(SHARED) $(LIBDIR)../../fpr -o $@ $(OBJS) $(FPR)
+	$(CC) $(LDFLAGS) $(RPATH) $(SHARED) $(LIBDIR)../../fpr -o $@ $(OBJS) $(FPR) $(LIBS)
 
 .c$(O):
 	$(CC) $(CFLAGS) $(PIC) $(INCDIR)../../fpr $(INCDIR)../../server $(INCDIR)../../external/stb -c -o $@ $<
--- module/fcgi/mod_fcgi.c
+++ module/fcgi/mod_fcgi.c
@@ -4,6 +4,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* https://www.mit.edu/~yandros/doc/specs/fcgi-spec.html */
+
+#define FCGI_HEADER_LEN 8
+
+#define FCGI_VERSION_1 1
+
+#define FCGI_BEGIN_REQUEST 1
+#define FCGI_ABORT_REQUEST 2
+#define FCGI_END_REQUEST 3
+#define FCGI_PARAMS 4
+#define FCGI_STDIN 5
+#define FCGI_STDOUT 6
+#define FCGI_STDERR 7
+#define FCGI_DATA 8
+#define FCGI_GET_VALUES 9
+#define FCGI_GET_VALUES_RESULT 10
+#define FCGI_UNKNOWN_TYPE 11
+#define FCGI_MAXTYPE (FCGI_UNKNOWN_TYPE)
+
+#define FCGI_NULL_REQUEST_ID 0
+
+#define FCGI_KEEP_CONN 1
+
+#define FCGI_RESPONDER 1
+#define FCGI_AUTHORIZER 2
+#define FCGI_FILTER 3
+
+#define FCGI_REQUEST_COMPLETE 0
+#define FCGI_CANT_MPX_CONN 1
+#define FCGI_OVERLOADED 2
+#define FCGI_UNKNOWN_ROLE 3
+
+static void send_packet(int fd, int type, int length, void* data) {
+	int	       pad = 8 - (length % 8);
+	unsigned char* pkt = malloc(8 + length + pad);
+
+	pkt[0] = 1; /* version 1 */
+	pkt[1] = type;
+	pkt[2] = 0;
+	pkt[3] = 0;
+	pkt[4] = (length >> 8) & 0xff;
+	pkt[5] = (length >> 0) & 0xff;
+	pkt[6] = pad;
+	pkt[7] = 0;
+	memcpy(pkt + 8, data, length);
+	memset(pkt + 8 + length, 0, pad);
+
+	fpr_send(fd, pkt, 8 + length + pad, 0);
+
+	free(pkt);
+}
+
 static int hook(fr_context_t* context, fr_request_t* req, fr_response_t* res) {
 	struct fpr_stat st;
 
